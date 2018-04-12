7c53f1db3f21eb0762346fa4c03d470898837a26
snowheat <musiklib@gmail.com>
2018-04-12 17:32:02 +0700

    beres sentences_words_array

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-import gensim.models.word2vec
+from gensim.models import word2vec
 
 
 def get_sentences_words_array():
-    pass
+
+    sentences_words_array = []
+
+    with open('id_gsd-ud-train.conllu', 'r', encoding="utf8") as t:
+
+        raw_sentences_array = t.read().split("\n\n")
+        for sentences_data in raw_sentences_array:
+            sentence = []
+            for word_data in sentences_data.splitlines()[2:]:
+                sentence.append(word_data.split("\t")[2])
+
+            sentences_words_array.append(sentence)
+
+    return sentences_words_array
 
 def get_word_vector():
     pass
@@ -16,4 +29,4 @@ def train():
 def test():
     pass
 
-
+print(get_sentences_words_array())
