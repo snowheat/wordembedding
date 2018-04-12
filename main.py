c3c5af6d6b84fd30af29edff681abf4dcdfd189e
sigitbn <sigitkariagil@gmail.com>
2018-04-12 18:21:55 +0700

    word2vec

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from gensim.models import word2vec
 
 
 def get_sentences_words_array():
-
-    sentences_words_array = []
+    sentences_words_array = [['']]
 
     with open('id_gsd-ud-train.conllu', 'r', encoding="utf8") as t:
 
@@ -17,16 +16,25 @@ def get_sentences_words_array():
 
     return sentences_words_array
 
-def get_word_vector():
+
+def get_word_vector(sentences):
+    model = word2vec.Word2Vec(sentences, size=2, window=2, min_count=1, workers=1)
+    return model
     pass
+
 
 def get_structured_data():
     pass
 
+
 def train():
     pass
+
 
 def test():
     pass
 
+
 print(get_sentences_words_array())
+sentences_list = get_sentences_words_array()
+print(get_word_vector(sentences_list))
