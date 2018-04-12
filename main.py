13df95a3cf20186d23a4fcdeab435fec8b100fcf
snowheat <musiklib@gmail.com>
2018-04-12 16:29:53 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import gensim.models.word2vec
+
+
+def get_sentences_words_array():
+    pass
+
+def get_word_vector():
+    pass
+
+def get_structured_data():
+    pass
+
+def train():
+    pass
+
+def test():
+    pass
+
+
